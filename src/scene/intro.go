ee0605532c69ae047e069bc294e4e6dbe67143a9
oprc <chisainux@gmail.com>
2025-04-20 00:28:53 +0700

    safe code lol

--- src/scene/intro.go
+++ src/scene/intro.go
@@ -1,8 +1,14 @@
 package scene
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"oprc_core/src/global"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func Intro() {
+	rl.BeginTextureMode(global.Rt2CurrentScene)
 	rl.ClearBackground(rl.Black)
-
+	// rl.OpenURL("https://github.com/gen2brain/raylib-go")
+	rl.EndTextureMode()
 }
